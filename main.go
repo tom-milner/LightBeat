f194975c6552fde48af9fec80025ebc856e0c23a
Tom <tom.f.milner@gmail.com>
2020-12-19 18:20:57 +0000

    LightBeat now runs automatically

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"main/spotify"
@@ -19,19 +20,66 @@ func main() {
 	}
 
 	log.Println("Starting ticker")
-	// Check for a song every 2 seconds.
+
+	lastPlaying := spotify.GetCurrentlyPlaying()
 	ticker := time.NewTicker(2 * time.Second)
+	var beatContex context.Context
+	var cancel context.CancelFunc
+	isPlaying := lastPlaying.IsPlaying
+	isDetecting := false
+
 	for {
 		<-ticker.C
 		currPlay := spotify.GetCurrentlyPlaying()
-		if currPlay.IsPlaying {
-			detectBeats(currPlay)
+
+		// If there has been a change in play state
+		if currPlay.IsPlaying != isPlaying {
+			log.Println("Change in play state")
+			isPlaying = currPlay.IsPlaying
+			if currPlay.IsPlaying {
+				log.Println("Starting")
+				beatContex, cancel = context.WithCancel(context.Background())
+				go detectBeats(beatContex, currPlay)
+				isDetecting = true
+			} else {
+				log.Println("Stopping")
+				cancel()
+				isDetecting = false
+				continue
+			}
 		}
+
+		playButNotDetect := (!isDetecting && currPlay.IsPlaying)
+		// If there has been a change in media
+		if currPlay.Item.ID != lastPlaying.Item.ID || playButNotDetect {
+
+			if !playButNotDetect {
+				log.Println("Change in media")
+				cancel()
+			} else {
+				log.Println("Playing without detection")
+			}
+			beatContex, cancel = context.WithCancel(context.Background())
+			go detectBeats(beatContex, currPlay)
+			lastPlaying = currPlay
+			isDetecting = true
+			log.Println("Starting")
+		}
+
 	}
+	// Check for a song every 2 seconds.
+
+	// for {
+	// 	<-ticker.C
+	// 	currPlay := spotify.GetCurrentlyPlaying()
+	// 	if currPlay.IsPlaying {
+	// 		detectBeats(currPlay)
+	// 	}
+	// }
 
 }
 
-func detectBeats(currPlay models.CurrentlyPlaying) {
+func detectBeats(ctx context.Context, currPlay models.CurrentlyPlaying) {
 	log.Println("Trackin beats.")
 	trackAn := spotify.GetTrackAnalysis(currPlay.Item.ID)
 
@@ -59,10 +107,15 @@ func detectBeats(currPlay models.CurrentlyPlaying) {
 
 	ticker := time.NewTicker(time.Duration(triggers[nextTrigger].Duration*1000) * time.Millisecond)
 	for nextTrigger < numTriggers-1 {
-		<-ticker.C
-		onTrigger(nextTrigger)
-		nextTrigger++
-		ticker = time.NewTicker(time.Duration(triggers[nextTrigger].Duration*1000) * time.Millisecond)
+		select {
+		case <-ticker.C:
+			onTrigger(nextTrigger)
+			nextTrigger++
+			ticker = time.NewTicker(time.Duration(triggers[nextTrigger].Duration*1000) * time.Millisecond)
+		case <-ctx.Done():
+			log.Println("Heard cancel. Exiting")
+			return
+		}
 	}
 }
 
