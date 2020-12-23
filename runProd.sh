3b0ea8bd71443c4908bf1d9b6c8e70e6cce5ae9a
Tom <tom.f.milner@gmail.com>
2020-12-23 21:30:03 +0000

    Added build script

--- runProd.sh
+++ runProd.sh
@@ -4,6 +4,12 @@ echo "Fetching latest from github..."
 git stash
 
 # Get latest from github
-git reset --hard FETCH_HEAD
+git pull origin master
+
+# Build Project
+go build 
+
+# Run!!
+./LightBeatGateway
 
 
